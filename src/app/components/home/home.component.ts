8b1aa3c630098d3c14ce34ba5aa81ae172f8b6b6
Nicolas <nico_keil@hotmail.com>
2021-03-25 21:54:42 -0300

    inmobiliaria

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../../models/post';
-import { PostService} from '../../services/post.service';
-import{ global } from '../../services/global';
+import { PostService } from '../../services/post.service';
+import { global } from '../../services/global';
 import { UserService } from '../../services/user.service';
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  providers: [PostService, UserService]
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
+	providers: [PostService, UserService]
 })
 export class HomeComponent implements OnInit {
 	public page_title: string;
@@ -16,45 +16,53 @@ export class HomeComponent implements OnInit {
 	public posts: Array<Post>;
 	public identity;
 	public token;
-  constructor(
-  	private _postService: PostService,
-  	private _userService: UserService
-  	) {
-  	this.page_title = 'Inicio';
-  	this.url = global.url
-  	this.identity = this._userService.getIdentity();
-  	this.token = this._userService.getToken();
-   }
+	public buscador: any;
+	public images1;
+	public images2;
+	public images3;
+	constructor(
+		private _postService: PostService,
+		private _userService: UserService
+	) {
+		this.page_title = 'Inicio';
+		this.url = global.url
+		this.identity = this._userService.getIdentity();
+		this.token = this._userService.getToken();
+	}
 
-  ngOnInit(): void {
-  	this.getPosts();
-  }
-  getPosts(){
-  	this._postService.getPost().subscribe(
-  		response => {
-  			if(response.status == 'success'){
-  				this.posts = response.post;
-  				
-  			}
-  			
-  		},
-  		error => {
+	ngOnInit(): void {
+		this.getPosts();
+	}
+	getPosts() {
+		this._postService.getPost().subscribe(
+			response => {
+				if (response.status == 'success') {
+					this.posts = response.post;
+					for (let i = 0; i < 3; i++) {
+						this.images1 = this.posts[0].image;
+						this.images2 = this.posts[0].image;
+						this.images3 = this.posts[0].image;
+					}
+				}
 
-  		}
-  		);
-  }
+			},
+			error => {
 
-  deletePost(id){
-    this._postService.delete(this.token, id).subscribe(
-        response => {
-            this.getPosts();
-            
-            
-          
-        },
-        error => {
-          console.log(error);
-        }
-      );
-  }
+			}
+		);
+	}
+
+	deletePost(id) {
+		this._postService.delete(this.token, id).subscribe(
+			response => {
+				this.getPosts();
+			},
+			error => {
+				console.log(error);
+			}
+		);
+	}
+	buscar(operacion, type, dor, zona) {
+		console.log(operacion, type, dor, zona);
+	}
 }
